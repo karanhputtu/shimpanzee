c9f7ef46e2dfdfae161012a42f09b2a1423c4465
karanhputtu <137089244+karanhputtu@users.noreply.github.com>
2023-06-19 20:59:35 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
 	public static void main(String[] args){
 		System.out.println("Hello, World!");
-		System.out.println("Welcome to Computer Science World")
+		System.out.println("Welcome to Software Engg world")
 	}
 }
 
